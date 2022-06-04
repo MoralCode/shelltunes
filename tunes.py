55ca6d3084ee38bd3c34c4e03370e4e4873ae914
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-06-04 15:02:33 -0400

    implement the rest of the basic stuff

--- tunes.py
+++ tunes.py
@@ -1,5 +1,31 @@
 import sys
 import math
+import random
+
+
+
+
+def rand_to_note(rand, samplerate):
+	"""converts a random integer to a note
+	"""
+	notes = [4, 5, 7, 11]
+	note_index = rand % 8
+	if note_index > len(notes) -1:
+		note = 0
+	else:
+		note = notes[note_index]/12
+	note = note * math.log(2)
+	note = math.exp(note)
+	note = note*1046
+	return note
+
+def note_to_frequency_special(note):
+	# this junk negates the math in tone_to_sine to get to a frequency value
+	note = note*samplerate/(2*math.pi)
+	return note
+
+
+
 
 # https://www.daniweb.com/programming/software-development/threads/354944/creating-a-sine-wave-at-a-specific-frequency-and-sample-rate
 def tone_to_sine(frequency:int, samplerate: int, duration:int, clean_end: bool):
@@ -25,7 +51,18 @@ def tone_to_sine(frequency:int, samplerate: int, duration:int, clean_end: bool):
 	for i in range(samplecount):
 		yield math.sin((frequency * (2* math.pi) * i) / samplerate)
 
+def apply_volume(generator, vol=100):
+	for i in generator:
+		yield int(vol * i)
+
+
 
 if __name__ == '__main__':
-	for line in sys.stdin:
-		pass
+	samplerate = 48000
+	note_duration = 1*samplerate
+	# while True:
+	randint = random.randint(0, 255)
+	note = rand_to_note(randint, samplerate)
+	freq = note_to_frequency_special(note)
+	for i in apply_volume(tone_to_sine(freq, samplerate, note_duration, True)):
+		print(i.to_bytes(4, byteorder='big', signed=True).hex())
